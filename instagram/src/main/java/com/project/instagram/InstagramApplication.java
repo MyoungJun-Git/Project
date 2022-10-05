469c30323ea232902848ac22a8e9611e8ed7382e
MJ <newwonmj@gmail.com>
2022-10-05 19:01:49 +0900

    Git 테스트

--- instagram/src/main/java/com/project/instagram/InstagramApplication.java
+++ instagram/src/main/java/com/project/instagram/InstagramApplication.java
@@ -7,9 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InstagramApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramApplication.class, args);
-
-		synchronized () {
-
-		}
 	}
 }
